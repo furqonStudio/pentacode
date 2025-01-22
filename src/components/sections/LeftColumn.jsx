b6b75fbfd8d5a7746ee7d09bafef5afaa52a271c
furqonstudio <furqonstudio@gmail.com>
2025-01-22 10:14:06 +0700

    style: fix LeftColumn flex

--- src/components/sections/LeftColumn.jsx
+++ src/components/sections/LeftColumn.jsx
@@ -3,22 +3,26 @@ import image1 from '../../assets/images/image1.png'
 const LeftColumn = () => {
   return (
     <div className="px-4 py-10">
-      <h1 className="font-gilroy text-sm-heading-1 sm:text-heading-1 font-semibold">
-        Kyiv
-        <br /> LuxeBouquets®
-      </h1>
-      <p className="font-gilroy italic mt-4 mb-6 text-sm">
-        Discover Uniquely Crafted Bouquets and Gifts for Any Occasion: Spread
-        Joy with Our Online Flower Delivery Service
-      </p>
-      <div className="border border-black" />
-      <div className="flex mt-4">
-        <img src={image1} alt="image1" className="h-52 object-cover" />
-        <div className="mx-4 h-52 border-l border-black" />
-        <p className="font-gilroy text-xs self-end">
-          Experience the joy of giving with our modern floral studio. Order
-          online and send fresh flowers, plants and gifts today.
+      <div className="flex flex-col gap-4 pb-6">
+        <h1 className="h1">
+          Kyiv
+          <br /> LuxeBouquets®
+        </h1>
+        <p className="italic text-sm font-normal">
+          Discover Uniquely Crafted Bouquets and Gifts for Any Occasion: Spread
+          Joy with Our Online Flower Delivery Service
         </p>
+      </div>
+      <div className="flex border-t border-black pt-4">
+        <div className=" pr-4 border-r border-black">
+          <img src={image1} alt="image1" className="h-[180px]" />
+        </div>
+        <div className="flex flex-1 pl-4">
+          <p className="text-[11px] font-normal self-end">
+            Experience the joy of giving with our modern floral studio. Order
+            online and send fresh flowers, plants and gifts today.
+          </p>
+        </div>
       </div>
     </div>
   )
