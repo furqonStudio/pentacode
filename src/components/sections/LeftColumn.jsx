2b9f1c53888a1c89a600022de4c60e6fd4903223
furqonstudio <furqonstudio@gmail.com>
2025-01-22 11:17:10 +0700

    feat(md): create md view for LeftColumn

--- src/components/sections/LeftColumn.jsx
+++ src/components/sections/LeftColumn.jsx
@@ -1,24 +1,28 @@
-import image1 from '../../assets/images/image1.png'
+import imageHero from '../../assets/images/image-hero.png'
 
 const LeftColumn = () => {
   return (
-    <div className="px-4 py-10">
+    <div className="px-4 py-10 md:p-20">
       <div className="flex flex-col gap-4 pb-6">
         <h1 className="h1">
           Kyiv
           <br /> LuxeBouquets®
         </h1>
-        <p className="italic text-sm font-normal">
+        <p className="italic text-sm md:text-lg font-normal">
           Discover Uniquely Crafted Bouquets and Gifts for Any Occasion: Spread
           Joy with Our Online Flower Delivery Service
         </p>
       </div>
       <div className="flex border-t border-black pt-4">
         <div className=" pr-4 border-r border-black">
-          <img src={image1} alt="image1" className="h-[180px]" />
+          <img
+            src={imageHero}
+            alt="Hero Image"
+            className="h-[180px] md:h-[256px] md:w-[280px]"
+          />
         </div>
         <div className="flex flex-1 pl-4">
-          <p className="text-[11px] font-normal self-end">
+          <p className="text-[11px] md:text-sm font-normal self-end">
             Experience the joy of giving with our modern floral studio. Order
             online and send fresh flowers, plants and gifts today.
           </p>
